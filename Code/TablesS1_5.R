d6bd48178f35a79829fce30a7ba08d532af7f4be
duncanobrien <duncan.a.obrien@gmail.com>
2023-03-01 11:06:28 +0000

    additional clarification to TablesS1_5

--- Code/TablesS1_5.R
+++ Code/TablesS1_5.R
@@ -11,21 +11,16 @@ load("Results/preprocessing_models.RData")
 ############ 
 #Extract model coefficients
 ############ 
+#drop first row (as is the reference none-none detrending and deseasoning method) and third column (as is jsut stating the credible interval value - i.e. 95%)
+tableS1 <- bayestestR::describe_posterior(ews.mod.detrend.mth.uni.roll, ci = 0.95, test="none")[-1,-3] 
 
-tableS1 <- bayestestR::describe_posterior(ews.mod.detrend.mth.uni.roll, ci = 0.95, test="none") |>
-  dplyr::select(-CI)
+tableS2 <- bayestestR::describe_posterior(ews.mod.detrend.mth.uni.exp, ci = 0.95, test="none")[-1,-3] 
 
-tableS2 <- bayestestR::describe_posterior(ews.mod.detrend.mth.uni.exp, ci = 0.95, test="none")|>
-  dplyr::select(-CI)
+tableS3 <- bayestestR::describe_posterior(ews.mod.detrend.mth.multi.roll, ci = 0.95, test="none")[-1,-3] 
 
-tableS3 <- bayestestR::describe_posterior(ews.mod.detrend.mth.multi.roll, ci = 0.95, test="none")|>
-  dplyr::select(-CI)
+tableS4 <- bayestestR::describe_posterior(ews.mod.detrend.mth.multi.exp, ci = 0.95, test="none")[-1,-3] 
 
-tableS4 <- bayestestR::describe_posterior(ews.mod.detrend.mth.multi.exp, ci = 0.95, test="none")|>
-  dplyr::select(-CI)
-
-tableS5 <- bayestestR::describe_posterior(ews.mod.detrend.mth.ml, ci = 0.95, test="none")|>
-  dplyr::select(-CI)
+tableS5 <- bayestestR::describe_posterior(ews.mod.detrend.mth.ml, ci = 0.95, test="none")[-1,-3] 
 
 ############ 
 #Export Tables
